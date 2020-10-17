9a0bc5fde5db4b9cc05776af97af3f49a29cf48a
Vladimir Okhrimenko <vovaokhrimenko1@gmail.com>
2020-10-17 09:24:05 +0300

    add variable

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "proxmox" {
     pm_tls_insecure = true
-    pm_api_url = "https://192.168.255.100:8006/api2/json"
-    pm_password = "kibitka228"
-    pm_user = "root@pam"
+    pm_api_url = var.api_url
+    pm_password = var.cloud_password
+    pm_user = var.cloud_user
     pm_otp = ""
 }
 
 resource "proxmox_vm_qemu" "ubuntu" {
-    name = "vm20"
+    name = "vm10"
     target_node = "tolik"
     clone = "ubuntu"
     os_type = "cloud-init"
---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "cloud_password" {
+    default = "kibitka228"
+} 
+
+variable "api_url" {
+    default = "https://192.168.255.100:8006/api2/json"
+}
+
+variable "cloud_user" {
+    default = "root@pam"
+}
